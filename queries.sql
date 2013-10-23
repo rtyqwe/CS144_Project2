ca2146b27c8943e0cfb337afb72c4281f24fb4b9
rtyqwe <ryanwmchan@icloud.com>
2013-10-23 12:19:06 -0700

    updated queries

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@ SELECT Count(UserID)
 FROM   User; 
 
 -- Find the number of sellers from "New York", (i.e., sellers whose location is exactly the string "New York"). Pay special attention to case sensitivity. You should match the sellers from "New York" but not from "new york".
-SELECT Count(UserID) 
+SELECT Count(User.UserID) 
 FROM   User 
        INNER JOIN Item 
                ON Item.UserID = User.UserID 
@@ -12,8 +12,8 @@ WHERE  BINARY User.Location = 'New York';
 -- Find the number of auctions belonging to exactly four categories.
 SELECT Count(*) 
 FROM   ItemCategory 
-GROUP  BY UserID 
-HAVING Count (UserID) = 4;
+GROUP  BY ItemID 
+HAVING (Count(ItemID) = 4);
 
 -- Find the ID(s) of current (unsold) auction(s) with the highest bid.
 SELECT ItemID 
@@ -21,14 +21,14 @@ FROM   Item
 WHERE  (SELECT ItemID 
         FROM   ItemBids 
         WHERE  Currently = (SELECT Max(Currently) 
-                            FROM   itembids)) 
+                            FROM   ItemBids)) 
        AND Ended > '2001-12-20 00:00:01'; 
 
 -- Find the number of sellers whose rating is higher than 1000.
 SELECT Count(*) 
-FROM   Users 
+FROM   User 
        INNER JOIN Item 
-               ON Item.UserID = USER.userid 
+               ON Item.UserID = User.UserID
 WHERE  Rating > 1000; 
 
 -- Find the number of users who are both sellers and bidders.
@@ -43,4 +43,3 @@ FROM   ItemCategory
 WHERE  ItemCategory.ItemID = (SELECT DISTINCT ItemID 
                               FROM   Bids 
                               WHERE  Amount > 100); 
-                              
