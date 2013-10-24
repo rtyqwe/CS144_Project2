98bee0b588e50ee8121629aa5a0bdc0a9f535382
rtyqwe <ryanwmchan@icloud.com>
2013-10-24 15:58:18 -0700

    Updated query 2, now 1, 2, 3, 7 have correct results 4, 5, 6 are slightly off.

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@ SELECT Count(1)
 FROM   User; 
 
 -- Find the number of sellers from "New York", (i.e., sellers whose location is exactly the string "New York"). Pay special attention to case sensitivity. You should match the sellers from "New York" but not from "new york".
-SELECT Count(User.UserID) 
+SELECT Count(DISTINCT User.UserID) 
 FROM   User 
        INNER JOIN Item 
                ON Item.UserID = User.UserID 
